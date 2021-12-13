39ddbdb39ce36e00cc97188ba382fadbde120546
Stone <stone.dummett@gmail.com>
2021-12-13 13:40:24 +0100

    Added Makefile and basic files to start working on the project

---
+++ philo/include/philosophers.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/13 12:59:54 by sdummett          #+#    #+#             */
+/*   Updated: 2021/12/13 13:09:25 by sdummett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+void	ft_strerror(char *str);
+int		ft_strlen(char *str);
+
+#endif
