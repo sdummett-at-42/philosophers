417e1cf2a18fbd450dbf2d1bb89d62d3355ec69c
Stone <stone.dummett@gmail.com>
2021-12-20 19:25:50 +0100

    Add gettimeofday function in order to get the time spent by the philo without eating

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 21:54:24 by sdummett          #+#    #+#             */
-/*   Updated: 2021/12/20 18:47:32 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/12/20 19:23:35 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,26 @@ void	*routine(void *arg)
 	t_philo		*philo;
 
 	philo = arg;
-	lock_speak_and_philo_is_alived(philo);
-	if (philo->datas->all_philo_is_alived)
-		printf("Hello from routine, I'm thread number %d\n", philo->id);
-	pthread_mutex_unlock(&philo->datas->speak_mutex);
-	pthread_mutex_unlock(&philo->datas->all_philo_is_alived_mutex);
-	pthread_mutex_lock(&philo->datas->all_philo_is_alived_mutex);
-	while (philo->datas->all_philo_is_alived)
+	printf("Hello from routine, I'm thread number %d\n", philo->id);
+	gettimeofday(&philo->start, NULL);
+	while (true)
 	{
-		usleep(philo->datas->time_to_sleep);
-		// pthread_mutex_unlock(&philo->datas->all_philo_is_alived_mutex);
-		// pthread_mutex_lock(&philo->datas->all_philo_is_alived_mutex);
-		break ;
+
+		pthread_mutex_lock(philo->datas->fork_mutex[philo->left_fork]);
+		pthread_mutex_lock(philo->datas->fork_mutex[philo->right_fork]);
+		gettimeofday(&philo->end, NULL);
+		if ((philo->end.tv_usec - philo->start.tv_usec) > philo->datas->time_to_die)
+		{
+			printf("Philo %d died, he has spent %ld without eating (ttd %d) :(\n",
+				philo->id, (philo->end.tv_usec - philo->start.tv_usec), philo->datas->time_to_die);
+			return arg;
+		}
+		else
+			usleep(philo->datas->time_to_eat);
+		pthread_mutex_unlock(philo->datas->fork_mutex[philo->left_fork]);
+		pthread_mutex_unlock(philo->datas->fork_mutex[philo->right_fork]);
+		gettimeofday(&philo->start, NULL);
+		usleep(philo->datas->time_to_sleep);		
 	}
-	pthread_mutex_unlock(&philo->datas->all_philo_is_alived_mutex);
 	return (arg);
 }
