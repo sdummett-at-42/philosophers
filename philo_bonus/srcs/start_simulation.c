66779bba07c849056535f8983f96aa82b21c2ad9
Stone <stone.dummett@gmail.com>
2021-12-29 21:03:08 +0100

    Delete useless printf

--- philo_bonus/srcs/start_simulation.c
+++ philo_bonus/srcs/start_simulation.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 21:06:00 by sdummett          #+#    #+#             */
-/*   Updated: 2021/12/29 14:30:53 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/12/29 21:00:25 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 int	start_simulation(t_philo *philo, char **args)
 {
 	if (!(init_simulation(philo, args)))
-	{
-		printf("get_simul failed\n");
 		return (0);
-	}
 	launch_processes(philo);
 	wait_processes_end(philo);
 	return (1);
