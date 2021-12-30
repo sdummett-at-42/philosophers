652b3352669c90240622241e066074768c1c19c4
Stone <stone.dummett@gmail.com>
2021-12-30 14:52:28 +0100

    Undo kill child from main process to make the philo program work at shool

--- philo_bonus/srcs/wait_processes_end.c
+++ philo_bonus/srcs/wait_processes_end.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 21:01:33 by sdummett          #+#    #+#             */
-/*   Updated: 2021/12/29 20:57:18 by sdummett         ###   ########.fr       */
+/*   Updated: 2021/12/30 14:49:26 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 void	wait_processes_end(t_philo *philo)
 {
-	int		i;
-	pid_t	pid;
+	int	i;
 
 	i = 0;
-	pid = waitpid(-1, NULL, 0);
 	while (i < philo->philo_number)
 	{
-		if (*(philo->pid) != pid)
-			kill(*(philo->pid), SIGTERM);
+		waitpid(philo->pid[i], NULL, 0);
 		i++;
 	}
 	sem_close(philo->forks_sem);
