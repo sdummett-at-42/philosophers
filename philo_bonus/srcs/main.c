fa8db910c8475cac6e6c6405cf0a6029a8d8dd4b
Stone <stone.dummett@gmail.com>
2022-01-03 15:18:50 +0100

    Norm

--- philo_bonus/srcs/main.c
+++ philo_bonus/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sdummett <sdummett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/24 18:38:55 by sdummett          #+#    #+#             */
-/*   Updated: 2022/01/03 14:56:09 by sdummett         ###   ########.fr       */
+/*   Updated: 2022/01/03 14:58:51 by sdummett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,5 @@ int	main(int ac, char **av)
 	memset(&philo, 0, sizeof(t_philo));
 	if (!(start_simulation(&philo, av + 1)))
 		printf("Something wrong happened\n");
-	// else
-	// 	printf(BMAG"End of the simulation\n"RESET);
-	// release_ressources(&philo);
 	return (0);
 }
